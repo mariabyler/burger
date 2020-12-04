a287767a74c4edc1b070861225c23b52dff15935
Maria Byler <mariabyler98@gmail.com>
2020-12-03 23:44:04 -0500

    routes

--- models/burger.js
+++ models/burger.js
@@ -1,16 +1,30 @@
-var orm = require('../config/orm');
+var orm = require("../config/orm");
 
-class Burger {
-    constructor(id, name, devoured){
-        this.id = id;
-        this.name = name;
-        this.devoured = devoured
-    }
+var burger = {
+  all: function(cb) {
+    orm.selectAll("burgers", function(res) {
+      cb(res);
+    });
+  },
+  // The variables cols and vals are arrays.
+  create: function(cb) {
+    orm.insertOne("burgers", 'burger_name', 'devoured', function(res) {
+      cb(res);
+    });
+  },
+  update: function(id, cb) {
+    var condition = "id =" + id;  
+    orm.updateOne("burgers", id, {devoured: true}, function(res) {
+      cb(res);
+    });
+  },
+//   delete: function(condition, cb) {
+//     orm.delete("burgers", condition, function(res) {
+//       cb(res);
+//     });
+//   }
+};
 
-    update(){
-    
-    }
-}
-
-module.exports = Burger; 
+// Export the database functions for the controller 
+module.exports = burger;
 
